02ce3c784c46a8e1709c86320760ee83f0043152
Malcolm Leake <echelon1089@gmail.com>
2020-06-09 03:02:38 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -6,6 +6,7 @@ export default class TwitterMessage extends React.Component {
 
     this.state = {
       tweet: ""
+<<<<<<< HEAD
 
     };
   }
@@ -16,6 +17,16 @@ export default class TwitterMessage extends React.Component {
      tweet: event.target.value
    })
 
+=======
+      maxTweet = props.maxChars
+    };
+  }
+
+  handleTweet= event => {
+    this.setState({
+      tweet: this.state.tweet.value
+    })
+>>>>>>> 6b9cdc481fa37b9c1e98ca805c38afb6b03348c2
   }
 
 
@@ -24,11 +35,15 @@ export default class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
+<<<<<<< HEAD
         <input type="text" name="message" id="message" value= {this.state.tweet} onChange = {event => this.handleTweet(event)}/>
         <h4> Words left
         {this.props.maxChars - this.state.tweet.length}
         </h4>
 
+=======
+        <input type="text" name="message" id="message" value= this.state.tweet />
+>>>>>>> 6b9cdc481fa37b9c1e98ca805c38afb6b03348c2
       </div>
     );
   }
